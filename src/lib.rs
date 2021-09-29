44bf6d438c9e0c3f30d9f381252e9d4af44bb23e
Timothy McCallum <tpmccallum@users.noreply.github.com>
2021-09-29 16:01:39 +1000

    Update first test function to be able to use wasmedge-extensions RunString

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,15 @@
-use wasm_bindgen::prelude::*;
 use pickledb::{PickleDb, PickleDbDumpPolicy, SerializationMethod};
+use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub fn first() {
+pub fn first() -> String {
     // create a new DB with AutoDump (meaning every change is written to the file)
     // and with Json serialization (meaning DB will be dumped to file as a Json object)
-    let mut db = PickleDb::new("example.db", PickleDbDumpPolicy::AutoDump, SerializationMethod::Json);
+    let mut db = PickleDb::new(
+        "example.db",
+        PickleDbDumpPolicy::AutoDump,
+        SerializationMethod::Json,
+    );
 
     // set the value 100 to the key 'key1'
     db.set("key1", &100).unwrap();
@@ -14,8 +18,13 @@ pub fn first() {
     println!("The value of key1 is: {}", db.get::<i32>("key1").unwrap());
 
     // load the DB from the same file
-    let db2 = PickleDb::load("example.db", PickleDbDumpPolicy::DumpUponRequest, SerializationMethod::Json).unwrap();
+    let db2 = PickleDb::load(
+        "example.db",
+        PickleDbDumpPolicy::DumpUponRequest,
+        SerializationMethod::Json,
+    )
+    .unwrap();
 
     // print the value of key1
-    println!("The value of key1 as loaded from file is: {}", db2.get::<i32>("key1").unwrap());
+    return db2.get::<i32>("key1").unwrap().to_string();
 }
